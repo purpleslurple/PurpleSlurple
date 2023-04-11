5915d90680ec3e6b6d251d9a4cc44e3d67ae33a3
Matthew Schneider <purpleslurple@gmail.com>
2023-04-11 14:55:20 -0500

    debugging... on production :)

--- ps.php
+++ ps.php
@@ -34,6 +34,7 @@ if (isset($_SERVER['REQUEST_URI']) && ($_SERVER['REQUEST_URI'] !="") ) {
     $file_location = $_SERVER['SCRIPT_NAME']; 
 } 
 $file_location = "https://".$_SERVER['SERVER_NAME'].$file_location;
+echo $file_location;
 
 // Register globals is bad, bad, bad - setting $theurl explicitly 
 $theurl = $_GET['theurl']; 
