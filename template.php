016c14b350877b0f423229b63a1b03d0709ca8f9
Matthew Schneider <purpleslurple@gmail.com>
2023-08-11 13:57:06 -0500

    Create template.php

---
+++ template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP with External JavaScript</title>
+    <script src="path/to/script.js"></script>
+</head>
+<body>
+
+<?php
+// Your PHP code here
+
+// ...
+
+?>
+
+</body>
+</html>
