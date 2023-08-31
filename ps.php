55a6af301c493c864691969f339ed17485918a11
Matthew Schneider <purpleslurple@gmail.com>
2023-08-31 10:28:24 +0000

    Change to match psArchive

--- ps.php
+++ ps.php
@@ -1,143 +1,130 @@
-<?php 
-// Config 
-// $show_header = true; 
-// $show_footer = true; 
-// End config 
+<?php
+// Config
+$show_header = true;
+$show_footer = true;
+// End config
 
-// Source code disclaimer - always added 
-// $ps_disclaimer = '<!-- 
-// PurpleSlurple Copyright 2002 by Matthew A. Schneider. 
-// PurpleSlurple code is licensed under the Open Software License version 1.1. 
-// This version was modified 12.12.2006 by 
-// Hans Fredrik Nordhaug <hans@nordhaug.priv.no>: 
-// - Made it work with register globals off (which is highly recommended). 
-// - Added autodetecting of location of this script. 
-// - Inserted header/disclaimer, style, base and footer without 
-//    creating invalid HTML/breaking existing package. 
-// - Added config section, might not be very useful. 
-// *************************************************************** 
-// * PurpleSlurple(TM) was created by Matthew A. Schneider       * 
-// * and was inspired by Purple, Augment, and others.            * 
-// * It was created ostensibly for the purpose of                * 
-// * facilitating my communication with Eric S. Raymond          * 
-// * regarding edits to his "How to Become a Hacker" document.   * 
-// * I\'m not kidding. You can\'t make this stuff up!              * 
-// *************************************************************** 
-// -->'; 
+// Source code disclaimer - always added
+$ps_disclaimer = '<!--
+PurpleSlurple Copyright 2002 by Matthew A. Schneider.
+PurpleSlurple code is licensed under the Open Software License version 1.1.
+This version was modified 12.12.2006 by
+Hans Fredrik Nordhaug <hans@nordhaug.priv.no>:
+- Made it work with register globals off (which is highly recommended).
+- Added autodetecting of location of this script.
+- Inserted header/disclaimer, style, base and footer without
+   creating invalid HTML/breaking existing package.
+- Added config section, might not be very useful.
+***************************************************************
+* PurpleSlurple(TM) was created by Matthew A. Schneider       *
+* and was inspired by Purple, Augment, and others.            *
+* It was created ostensibly for the purpose of                *
+* facilitating my communication with Eric S. Raymond          *
+* regarding edits to his "How to Become a Hacker" document.   *
+* I\'m not kidding. You can\'t make this stuff up!              *
+***************************************************************
+-->';
 
-// Automatically detect the location of this file 
-// if (isset($_SERVER['REQUEST_URI']) && ($_SERVER['REQUEST_URI'] !="") ) { 
-//     $file_location = $_SERVER['REQUEST_URI']; 
-// } else if (isset($_SERVER['PHP_SELF']) && ($_SERVER['PHP_SELF'] !="") ) { 
-//     $file_location = $_SERVER['PHP_SELF']; 
-// } else { 
-//     $file_location = $_SERVER['SCRIPT_NAME']; 
-// } 
-// $file_location = "https://".$_SERVER['SERVER_NAME'].$file_location;
-$file_location = "https://purpleslurple.com/ps.php";
+// Automatically detect the location of this file
+if (isset($_SERVER['PATH_INFO']) && ($_SERVER['PATH_INFO'] !="") ) {
+    $file_location = $_SERVER['PATH_INFO'];
+} else if (isset($_SERVER['PHP_SELF']) && ($_SERVER['PHP_SELF'] !="") ) {
+   $file_location = $_SERVER['PHP_SELF'];
+} else {
+   $file_location = $_SERVER['SCRIPT_NAME'];
+}
+$file_location = "https://".$_SERVER['HTTP_HOST'].$file_location;
 
-// check if the 'theurl' parameter is set
-if (!isset($_GET['theurl'])) {
-    // handle the error here
-    echo "theurl parameter is not set";
+// If set, get the url to slurp
+if (isset($_GET['theurl'])) {
+    $theurl = $_GET['theurl'];
+} else {
+    show_welcome();
+}
+
+function show_welcome() {
+    global $file_location;
+    echo '
+<title>PurpleSlurple</title>
+<h2>Welcome to PurpleSlurple &#153;</h2>
+<h3>Granular Addressability in HTML Documents - ON THE FLY</h3>
+<p><b><q>Slurp up a Web page, spit back Purple numbers</q></b></p><hr>
+<p>If you are not familiar with Purple numbers you may want to read Eugene Eric Kim\'s &ldquo;
+<a href="http://www.eekim.com/software/purple/purple.html">An Introduction to Purple</a>&rdquo;.
+See also Eric Armstrong\'s comments on <a href="'.$file_location.
+'?theurl=https://web.archive.org/web/20020705201817/http://www.treelight.com/software/collaboration/whatsWrongWithEmail.html#purp720">granular addressability</a></p>
+<p>Want one-click Purple numbers? Right-click on this link,
+<a href="javascript:location.href=\''.$file_location.
+'?theurl=\'+document.location.href;">PurpleSlurple Bookmarklet</a>,
+and bookmark it, or drag and drop this bookmark onto your browser\'s personal toolbar.
+Now when you are viewing a page on which you would like Purple numbers just click the bookmarklet.
+(Javascript must be enabled).</p><hr>
+<p>Enter the URL of the page to which you would like to apply Purple numbers.</p>
+<form method="get" action="'.$_SERVER['SCRIPT_NAME'].'"><input type="text" name="theurl" size="30">
+(e.g., https://somedomain.com/somepage.html)<br><input type="submit" value="Submit"></form>
+<hr><p><a href="https://purpleslurple.com/">PurpleSlurple</a> &#153;
+was created by <a href="mailto:matsch@sasites.com">Matthew A. Schneider</a></p>';
+  exit;
+}
+
+// Do not slurp self
+if (strpos($theurl,$file_location) !== false)
+     die('PurpleSlurple won\'t slurp itself :-)'); //die, do not process
+
+// PurpleSlurple header/disclaimer and expand / collapse link
+$ps_header = '<h1>This page was generated by <a href="'.$file_location.'">PurpleSlurple</a>&#153;.
+The original page can be found <a href="'.$theurl.'">here</a>.</h1><hr>';
+
+// PurpleSlurple footer
+$ps_footer = '<br style="clear:both"><hr><p style="height: 700px">
+<a href="https://purpleslurple.com/">PurpleSlurple</a>&#153; was created
+by <a href="mailto:matsch@sasites.com">Matthew A. Schneider</a></p>';
+
+// set base to ensure relative links work
+// Thanks to http://marc.theaimsgroup.com/?l=php-general&m=95597547227951&w=2  Duh!
+$ps_base = "<base href='$theurl'>";
+
+// collapse outline (hiding elements)
+$ps_style = "<style type='text/css'>p {display:none}\nli {display:none}\n</style>\n";
+
+// Slurp the page
+// Accept https URLs only
+if (strpos($theurl,"https://") !== 0) {
+    echo "<h1>PurpleSlurple only slurps https:// protocol URLS. $theurl is invalid.</h1>";
     exit;
 }
-
-$theurl = $_GET['theurl'];
-
-// check for target URL, if none present PS form 
-if (!($theurl)) 
-{ 
-    echo ' 
-<title>PurpleSlurple</title> 
-<h2>Welcome to PurpleSlurple &#153;</h2> 
-<h3>Granular Addressability in HTML Documents - ON THE FLY</h3> 
-<p><b><q>Slurp up a Web page, spit back Purple numbers</q></b></p><hr> 
-<p>If you are not familiar with Purple numbers you might want to read Eugene Eric Kim\'s &ldquo; 
-<a href="https://web.archive.org/web/20021102203338/http://www.eekim.com/software/purple/purple.html">An Introduction to Purple</a>&rdquo;. 
-See also Eric Armstrong\'s comments on <a href="">granular addressability</a></p> 
-<p>Want one-click Purple numbers? Right-click on this link, 
-<a href="javascript:location.href=\''.$file_location. 
-'?theurl=\'+document.location.href;">PurpleSlurple Bookmarklet</a>, 
-and bookmark it, or drag and drop this bookmark onto your browser\'s personal toolbar. 
-Now when you are viewing a page on which you would like Purple numbers just click the bookmarklet. 
-(Javascript must be enabled).</p><hr> 
-<p>Enter the URL of the page to which you would like to apply Purple numbers.</p> 
-<form method="get" action="ps.php"><input type="text" name="theurl" size="30"> 
-(e.g., https://www.somedomain.com/somepage.html)<br><input type="submit" value="Submit"></form> 
-<hr><p><a href="https://purpleslurple.com/ps.php">PurpleSlurple</a> &#153; 
-was created by <a href="mailto:matsch@sasites.com">Matthew A. Schneider</a></p>'; 
-  exit; 
-} 
-
-// Do not slurp self 
-// if (strpos($theurl,$file_location) !== false) 
-//      die('PurpleSlurple won\'t slurp itself :-)'); //die, do not process 
-
-// PurpleSlurple header/disclaimer and expand / collapse link 
-// $ps_header = '<p>This page was generated by <a href="'.$file_location.'">PurpleSlurple</a>&#153;. 
-// The original page can be found <a href="'.$theurl.'">here</a>.</p><br><a href="'.$file_location.'?collapse=no&amp;theurl='.$theurl.'">expand</a>&nbsp;|&nbsp 
-// <a href="'.$file_location.'?collapse=yes&amp;theurl='.$theurl.'">collapse</a><p><hr>'; 
-// // The following isn't displayed since the collapse function really doesn't 
-// // work as intended - as far as I can tell. (Hans Nordhaug, 12.05.2007.) 
-// $ps_header_unused = '<br><a href="'.$file_location.'?collapse=no&amp;theurl='.$theurl.'">expand</a>&nbsp;|&nbsp 
-// <a href="'.$file_location.'?collapse=yes&amp;theurl='.$theurl.'">collapse</a><p><hr>'; 
-
-// PurpleSlurple footer 
-// $ps_footer = '<br style="clear:both"><hr><p style="height: 700px"> 
-// <a href="https://purpleslurple.com/ps.php">PurpleSlurple</a>&#153; was created 
-// by <a href="mailto:matsch@sasites.com">Matthew A. Schneider</a></p>'; 
-
-// set base to ensure relative links work 
-// Thanks to http://marc.theaimsgroup.com/?l=php-general&m=95597547227951&w=2  Duh! 
-// $ps_base = "<base href='$theurl'>"; 
-
-// $html_content = file_get_contents($theurl);
-$html_content = file_get_contents($theurl);
-if ($html_content === false) {
-    // handle the error here
-    echo "file_get_contents error";
+$fcontents = @file($theurl);
+if (!$fcontents) {
+    echo "<h1>Could not open $theurl</h1>";
     exit;
 }
-
-
-if ($html_content !== false) {
-    // create a new DOMDocument instance
-    $dom = new DOMDocument();
+$theurl = urlencode($theurl);
+$ps_contents = "";
+foreach ($fcontents as $line_num => $line) {
+    $pattern = "/<p[^>]*>|<h[1-6][^>]*>|<li[^nk>]*>/i";
+    $replacement = "\\0(<a href='$file_location?theurl=$theurl#purp$line_num' id='purp$line_num'><font color='purple'>$line_num</font></a>) ";
+    $ps_contents .= preg_replace($pattern, $replacement, $line);
 }
 
-// suppress warnings for invalid HTML
-libxml_use_internal_errors(true);
+// find head and body and insert disclaimer/header/footer/style/base
+list($head,$body) = explode("</head>", $ps_contents);
+if (isset($_GET['collapse']) && ($_GET['collapse'] == "yes")) {
+    $head = str_replace("<head>","<head>\n$ps_style", $head);;
+}
+if (!strpos("<base",$head)) {
+    $head = str_replace("<head>","<head>\n$ps_base", $head);;
+}
 
-// load the HTML content into the DOMDocument
-$dom->loadHTML($html_content);
+// insert disclaimer/header/footer
+$head = str_replace("<head>","<head>\n$ps_disclaimer", $head);
+if ($show_header) {
+    $body = preg_replace("/<body[^>]*>/i","\\0\n$ps_header",$body);
+}
+if ($show_footer) {
+    $body = str_replace("</body>","$ps_footer\n</body>",$body);
+}
 
-// restore error reporting
-libxml_use_internal_errors(false);
-    
-    // find all the relevant HTML tags (p, h1-h6, li)
-    $tags = $dom->getElementsByTagName('p');
-    $tags = array_merge($tags, $dom->getElementsByTagName('h1'));
-    $tags = array_merge($tags, $dom->getElementsByTagName('h2'));
-    $tags = array_merge($tags, $dom->getElementsByTagName('h3'));
-    $tags = array_merge($tags, $dom->getElementsByTagName('h4'));
-    $tags = array_merge($tags, $dom->getElementsByTagName('h5'));
-    $tags = array_merge($tags, $dom->getElementsByTagName('h6'));
-    $tags = array_merge($tags, $dom->getElementsByTagName('li'));
-    
-    // loop through each tag and modify its content
-    foreach ($tags as $tag) {
-        $line_num = $tag->getLineNo();
-        $tag_content = $tag->ownerDocument->saveXML($tag);
-        $pattern = "/<p[^>]*>|<h[1-6][^>]*>|<li[^nk>]*>/i";
-        $replacement = "$tag_content(<a href='$file_location?theurl=$theurl#purp$line_num' name='purp$line_num'><font color='purple'>$line_num</font></a>) ";
-        $new_tag_content = preg_replace($pattern, $replacement, $tag_content);
-        $new_tag = $dom->createDocumentFragment();
-        $new_tag->appendXML($new_tag_content);
-        $tag->parentNode->replaceChild($new_tag, $tag);
-    }
-    
-    // get the modified HTML content from the DOMDocument
-    $modified_html_content = $dom->saveHTML();
-    echo $modified_html_content;
+// Sending result to browser
+echo $head."</head>".$body;
+
 ?>
